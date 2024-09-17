a6de780b6a59f9be4a5176a139be11afc9384994
Nerijus <Nerijus@DESKTOP-08FUU9C>
2024-09-17 22:27:29 +0300

    Add in program razor page app.MapRazorPages();

--- DevSpot/Program.cs
+++ DevSpot/Program.cs
@@ -42,6 +42,8 @@ namespace DevSpot
 
             app.UseAuthorization();
 
+            app.MapRazorPages();
+
             app.MapControllerRoute(
                 name: "default",
                 pattern: "{controller=Home}/{action=Index}/{id?}");

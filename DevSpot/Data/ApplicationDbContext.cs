9b750b3cfed8a8c242f60a4610d528e3d56c198f
Nerijus <Nerijus@DESKTOP-08FUU9C>
2024-09-17 07:13:24 +0300

    Add project files.

---
+++ DevSpot/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace DevSpot.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContextDbC(DbContextOptions<ApplicationDbContext> options) :
+            base(options)
+        {
+            
+        }
+    }
+}

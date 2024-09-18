d03d19a347b95a5164964f8c3f5c362755c36f4a
Nerijus <Nerijus@DESKTOP-08FUU9C>
2024-09-18 22:45:51 +0300

    .

--- DevSpot/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ DevSpot/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -97,6 +97,7 @@ namespace DevSpot.Areas.Identity.Pages.Account
             [Display(Name = "Confirm password")]
             [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
             public string ConfirmPassword { get; set; }
+            public bool IsJobSeeker { get; set; } = true;   
         }
 
 

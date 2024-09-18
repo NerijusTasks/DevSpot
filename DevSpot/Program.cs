cb6205a43c9b95a7d8812737b70bf28864ea54b5
Nerijus <Nerijus@DESKTOP-08FUU9C>
2024-09-18 21:46:36 +0300

    Add two another Roles

--- DevSpot/Program.cs
+++ DevSpot/Program.cs
@@ -1,3 +1,4 @@
+using DevSpot.Constants;
 using DevSpot.Data;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
@@ -38,13 +39,9 @@ namespace DevSpot
             using (var scope = app.Services.CreateScope())
             {
                 var services = scope.ServiceProvider;
-                var roleManager = services.GetRequiredService<RoleManager<IdentityRole>> ();
 
-                if (!roleManager.RoleExistsAsync("Admin").Result)
-                {
-                   var result = roleManager.CreateAsync(new IdentityRole("Admin")).Result;
-                }
-                
+                RoleSeeder.SeedRolesAsync(services).Wait();
+
             }
             app.UseHttpsRedirection();
             app.UseStaticFiles();

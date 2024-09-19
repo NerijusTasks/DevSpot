324386b22687eea63944b7c9a0c758e0f48fd82e
Nerijus <Nerijus@DESKTOP-08FUU9C>
2024-09-19 22:28:00 +0300

    Create interface and class with methods

---
+++ DevSpot/Repositories/JobPostingRepository.cs
@@ -0,0 +1,39 @@
+﻿
+using DevSpot.Data;
+using DevSpot.Models;
+
+namespace DevSpot.Repositories
+{
+    public class JobPostingRepository : IRepository<JobPosting>
+    {
+        private readonly ApplicationDbContext _context;
+        public JobPostingRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public Task AddAsync(JobPosting entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<JobPosting>> GetAllAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<JobPosting> GetByIdAsinc(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Updateasync(JobPosting entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

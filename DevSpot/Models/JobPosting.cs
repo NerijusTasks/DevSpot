e4fa54b38eadb04fa94928c8c20057646a89eafc
Nerijus <Nerijus@DESKTOP-08FUU9C>
2024-09-19 21:35:27 +0300

    Add new model

---
+++ DevSpot/Models/JobPosting.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DevSpot.Models
+{
+    public class JobPosting
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Title { get; set; }
+        [Required]
+        public string Descriptio { get; set; }
+        [Required]
+        public string Company { get; set; }
+        [Required]
+        public string Location { get; set; }
+        public DateTime PostedDate { get; set; } = DateTime.UtcNow;
+        public bool IsApproved { get; set; }
+        public string UserId { get; set; }
+    }
+}
